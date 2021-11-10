dbf5e3c03df53beae66c4fe136074c90232ccf4b
emonislam079 <emonislam079@gmail.com>
2021-11-10 13:11:12 +0600

    firebase

---
+++ src/Components/LogIn/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB2S4UkBGHVqpp3HSGtCWsLvhlUWc5tlWc",
+  authDomain: "toys-zone-ef3aa.firebaseapp.com",
+  projectId: "toys-zone-ef3aa",
+  storageBucket: "toys-zone-ef3aa.appspot.com",
+  messagingSenderId: "684264052279",
+  appId: "1:684264052279:web:8f0fb1f5d5331fceabd1b2"
+  };
+  
+
+  export default firebaseConfig;
